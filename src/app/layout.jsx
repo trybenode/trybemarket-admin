ead2fd97925ab22c9d9a050f9685cb313d6e598e
Abdul1013 <abdulhaqabdulrasheed@gmail.com>
2025-11-22 00:23:59 +0100

    used layout logic

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,18 @@
-import './globals.css'
+import "./globals.css";
+import Header from "../components/Header";
+import SideNav from "../components/SideNav";
 
 export const metadata = {
-  title: 'TrybeMarket Admin',
-  description: 'Admin dashboard for TrybeMarket',
-}
+  title: "TrybeMarket Admin",
+  description: "Admin dashboard for TrybeMarket",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header />
+      <SideNav />
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,9 @@
+
+
 export default function Home() {
   return (
     <div className="text-center">
-      <h1 className="text-4xl font-bold">Welcome to My App</h1>
-      <p className="mt-4 text-lg">This is a simple React application styled with Tailwind CSS.</p>
+     
     </div>
   )
 }
